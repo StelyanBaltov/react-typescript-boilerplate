a279494690ba17dc63cd6aeef5ba77d975cdcc8a
stelyan.baltov <stelyan.baltov@egt-interactive.com>
2024-04-26 15:37:39 +0300

    Use the carousel as external lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import styles from './App.module.css'
+import { CarouselContainer } from 'react-carousel-infinite-scroll-ts'
+import { carouselData } from './data/carouselData'
 
 const App: React.FC = () => {
-    const { container } = styles
+    const { container, carousel, item, bigContainer } = styles
 
-    return <div className={container}>Hello World</div>
+    return (
+        <div className={container}>
+            <CarouselContainer items={carouselData} carouselClassName={carousel} visibleElementsCount={5} />
+            <CarouselContainer
+                items={carouselData}
+                carouselClassName={bigContainer}
+                visibleElementsCount={10}
+                itemClassName={item}
+            />
+        </div>
+    )
 }
 
 export default App
---
+++ src/data/carouselData.ts
@@ -0,0 +1,44 @@
+export interface CarouselDataItem {
+    src: string
+}
+
+export const carouselData: CarouselDataItem[] = [
+    { src: 'https://winghamwildlifepark.co.uk/wp-content/uploads/2013/04/Panda-200-300x300.jpg' },
+    { src: 'https://cdn.firstcry.com/education/2022/11/26141737/Animal-Name-Starting-With-L-For-Kids.jpg' },
+    {
+        src: 'https://foothillsanimalshelter.org/wp-content/uploads/2024/04/Screenshot-2024-04-09-at-3.30.02-PM-300x300.png',
+    },
+    { src: 'https://lafeber.com/pet-birds/wp-content/uploads/2018/06/Zebra-Finch-300x300.jpg' },
+    {
+        src: 'https://2.bp.blogspot.com/-WPfHNmLnIyM/VbHUfJl9Q7I/AAAAAAAAB-U/2ToZPKOd4t4/s1600/baby-animals-hd-wallpapers-dowload.jpg',
+    },
+    { src: 'https://www.mammal.org.uk/wp-content/uploads/2021/09/red-fox-300x300.jpg' },
+    { src: 'https://www.allthebestpetcare.com/wp-content/uploads/2021/10/cbd-oil-cat-anxiety-300x300.jpg' },
+    { src: 'https://fairtradesafaris.com/wp-content/uploads/2021/04/1044927_FTS-15birds-5_042121-300x300.png' },
+    { src: 'https://petkingdom.org/wp-content/uploads/2024/04/F-Cavpo-176-300x300.jpg' },
+    { src: 'https://www.animalkingdomaz.com/wp-content/uploads/adult-pomsky-1-300x300.jpg' },
+    { src: 'https://winghamwildlifepark.co.uk/wp-content/uploads/2013/04/Panda-200-300x300.jpg' },
+    { src: 'https://i.pinimg.com/400x300/07/a5/86/07a586a38cd7df6f2b1187e6d5aa63ff.jpg' },
+    {
+        src: 'https://foothillsanimalshelter.org/wp-content/uploads/2024/04/Screenshot-2024-04-09-at-3.30.02-PM-300x300.png',
+    },
+    { src: 'https://lafeber.com/pet-birds/wp-content/uploads/2018/06/Zebra-Finch-300x300.jpg' },
+    { src: 'https://www.mammal.org.uk/wp-content/uploads/2021/12/Untitled-design4-300x300.jpg' },
+    { src: 'https://www.mammal.org.uk/wp-content/uploads/2021/09/red-fox-300x300.jpg' },
+    { src: 'https://www.allthebestpetcare.com/wp-content/uploads/2021/10/cbd-oil-cat-anxiety-300x300.jpg' },
+    { src: 'https://fairtradesafaris.com/wp-content/uploads/2021/04/1044927_FTS-15birds-5_042121-300x300.png' },
+    { src: 'https://petkingdom.org/wp-content/uploads/2024/04/F-Cavpo-176-300x300.jpg' },
+    { src: 'https://www.animalkingdomaz.com/wp-content/uploads/adult-pomsky-1-300x300.jpg' },
+    { src: 'https://winghamwildlifepark.co.uk/wp-content/uploads/2013/04/Panda-200-300x300.jpg' },
+    { src: 'https://i.pinimg.com/400x300/07/a5/86/07a586a38cd7df6f2b1187e6d5aa63ff.jpg' },
+    {
+        src: 'https://foothillsanimalshelter.org/wp-content/uploads/2024/04/Screenshot-2024-04-09-at-3.30.02-PM-300x300.png',
+    },
+    { src: 'https://lafeber.com/pet-birds/wp-content/uploads/2018/06/Zebra-Finch-300x300.jpg' },
+    { src: 'https://www.mammal.org.uk/wp-content/uploads/2021/12/Untitled-design4-300x300.jpg' },
+    { src: 'https://www.mammal.org.uk/wp-content/uploads/2021/09/red-fox-300x300.jpg' },
+    { src: 'https://www.allthebestpetcare.com/wp-content/uploads/2021/10/cbd-oil-cat-anxiety-300x300.jpg' },
+    { src: 'https://fairtradesafaris.com/wp-content/uploads/2021/04/1044927_FTS-15birds-5_042121-300x300.png' },
+    { src: 'https://petkingdom.org/wp-content/uploads/2024/04/F-Cavpo-176-300x300.jpg' },
+    { src: 'https://www.animalkingdomaz.com/wp-content/uploads/adult-pomsky-1-300x300.jpg' },
+]
