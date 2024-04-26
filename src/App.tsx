46ae65d2188b17f200e0dfd17f22610495dc8f9b
stelyan.baltov <stelyan.baltov@egt-interactive.com>
2024-04-26 15:49:24 +0300

    Add title

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { CarouselContainer } from 'react-carousel-infinite-scroll-ts'
 import { carouselData } from './data/carouselData'
 
 const App: React.FC = () => {
-    const { container, carousel, item, bigContainer } = styles
+    const { container, carousel, item, bigContainer, label } = styles
 
     return (
         <div className={container}>
+            <div className={label}>Carousel Examples</div>
             <CarouselContainer items={carouselData} carouselClassName={carousel} visibleElementsCount={5} />
+            <CarouselContainer items={carouselData} visibleElementsCount={10} />
             <CarouselContainer
                 items={carouselData}
                 carouselClassName={bigContainer}
-                visibleElementsCount={10}
+                visibleElementsCount={20}
                 itemClassName={item}
             />
         </div>
